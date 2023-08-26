dc0689243dfd918c3d1d04fe9d8b37508a5acf03
Ilya Malashenko <melax08@yandex.ru>
2023-08-26 14:12:24 +0400

    Refactor code, add Answer.content validation, add logic for qualities sharing third place, add more tests

--- src/backend/api/calculation_service/task_1.py
+++ src/backend/api/calculation_service/task_1.py
@@ -1,7 +1,22 @@
+import logging
+
+from rest_framework.serializers import ValidationError
+
 from api.models import Answer
 
 LETTERS: tuple[str, ...] = ('А', 'Б', 'В', 'Г', 'Д', 'Е')
 ANSWER_OPTIONS_COUNT: int = 6
+
+
+def _validate_answer(answer: Answer) -> None:
+    """Проверяет поле Answer.content на консистентность данных."""
+    if answer.content is None or len(answer.content) != ANSWER_OPTIONS_COUNT:
+        error_message = (
+            f'Размер Answer.content не равен {ANSWER_OPTIONS_COUNT}. '
+            f'ID ответа: {answer.id}'
+        )
+        logging.error(error_message)
+        raise ValidationError({'errors': error_message})
 
 
 def calculate_task_1_result(user_answers: list[Answer]) -> str:
@@ -13,12 +28,23 @@ def calculate_task_1_result(user_answers: list[Answer]) -> str:
     points = [0] * ANSWER_OPTIONS_COUNT
 
     for answer in user_answers:
-        answer_points = list(map(int, answer.content))
-        points = [
-            points[idx] + answer_points[idx]
-            for idx in range(len(answer_points))
-        ]
+        _validate_answer(answer)
+        answer_points = tuple(map(int, answer.content))
+        for index, value in enumerate(answer_points):
+            points[index] += value
 
-    results = sorted(zip(points, LETTERS))[::-1]
-    summary = [result[1] for result in results[:3]]
-    return ''.join(summary)
+    results = sorted(zip(points, LETTERS), reverse=True)
+
+    # Формируем summary из трех результатов, набравших больше всего баллов.
+    summary = [f'{result[1]};{result[0]}' for result in results[:3]]
+
+    # Если третье место делят несколько качеств, записываем их тоже.
+    third_place_scores = results[2][0]
+
+    for result in results[3:]:
+        if result[0] == third_place_scores:
+            summary.append(f'{result[1]};{result[0]}')
+        else:
+            break
+
+    return '\n'.join(summary)
--- src/backend/api/calculation_service/test_task_2.py
+++ src/backend/api/calculation_service/test_task_2.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from rest_framework.serializers import ValidationError
 
 from api.models import Answer
 from .task_1 import calculate_task_1_result
@@ -12,7 +13,7 @@ class TestTask1(TestCase):
         """Правильно определилась расшифровка с одинаковым ответом на все
         вопросы по первому заданию."""
         answers = [Answer(content='012345') for _ in range(10)]
-        self.assertEqual('ЕДГ', calculate_task_1_result(answers))
+        self.assertEqual('Е;50\nД;40\nГ;30', calculate_task_1_result(answers))
 
     def test_task_1_different_answers(self):
         """Правильно определилась расшифровка с различными ответами
@@ -22,4 +23,45 @@ class TestTask1(TestCase):
             '321054', '013245', '543012', '230451'
         )
         answers = [Answer(content=answer) for answer in answers_content]
-        self.assertEqual('ЕДБ', calculate_task_1_result(answers))
+        self.assertEqual('Е;31\nД;29\nБ;27', calculate_task_1_result(answers))
+
+    def test_task_1_inconsistent_answer_content(self):
+        """Неконсистентный Answer.content прервал выполнение проверки."""
+        with self.assertRaises(ValidationError):
+            calculate_task_1_result([Answer(content='12345')])
+
+        with self.assertRaises(ValidationError):
+            calculate_task_1_result([Answer(content=None)])
+
+        # Консистентный Answer.content был посчитан корректно
+        self.assertEqual(
+            'Е;5\nД;4\nГ;3',
+            calculate_task_1_result([Answer(content='012345')])
+        )
+
+    def test_task_1_more_than_one_third_place(self):
+        """Правильно определилась расшифровка с одинаковым количеством баллов
+        у третьего, четвертого и остальных мест по первому заданию."""
+        self.assertEqual(
+            'Е;5\nД;5\nГ;5\nВ;5\nБ;5\nА;5',
+            calculate_task_1_result([Answer(content='555555')])
+        )
+        self.assertEqual(
+            'Е;5\nД;5\nГ;5\nВ;5',
+            calculate_task_1_result([Answer(content='115555')])
+        )
+
+        # Контент соответствующий реальным ответам на тесты,
+        # но третье место делят сразу два качества.
+        real_ten_answers_content = (
+            '243510', '053142', '123504', '041235', '012345', '410523',
+            '321054', '013245', '543012', '230451'
+        )
+        answers = [
+            Answer(content=answer) for answer in real_ten_answers_content
+        ]
+
+        self.assertEqual(
+            'Е;31\nД;29\nГ;27\nБ;27',
+            calculate_task_1_result(answers)
+        )
