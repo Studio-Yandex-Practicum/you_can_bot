9f4f22d0e47f9f5cdc3e5e8e032a217d52980101
Ilya <99960381+melax08@users.noreply.github.com>
2023-08-26 17:59:30 +0400

    Add function to calculate first task answers (#16)
    * Add function to calculate first task answers, add tests for this function
    
    * Refactor code, add Answer.content validation, add logic for qualities sharing third place, add more tests
    
    * Add logging settings for Django.

--- src/bot/utils/configs.py
+++ src/bot/utils/configs.py
@@ -18,12 +18,14 @@ TOKEN = os.getenv("TOKEN")
 
 BASE_DIR = Path(__file__).parent.parent
 
-LOG_DIR = BASE_DIR / os.getenv("LOG_DIR", default="logs")
-LOG_FILE_PATH = LOG_DIR / os.getenv("LOG_FILE_NAME", default="tg_you_can.log")
+LOG_DIR = BASE_DIR.parent / ".data" / os.getenv("LOG_DIR", default="logs")
+LOG_FILE_PATH = LOG_DIR / "bot.log"
 LOG_LEVEL = LOG_LEVELS.get(os.getenv("LOG_LEVEL", default="INFO"), logging.INFO)
-LOG_FORMAT = "%(asctime)s - [%(levelname)s] - %(message)s"
-LOG_DT_FORMAT = "%d.%m.%Y %H:%M:%S"
-LOG_BACKUP_COUNT = 14
+
+LOG_FORMAT = "[%(asctime)s,%(msecs)d] %(levelname)s [%(name)s:%(lineno)s] %(message)s"
+LOG_DT_FORMAT = "%d.%m.%y %H:%M:%S"
+
 LOG_WHEN = "midnight"
 LOG_INTERVAL = 1
+LOG_BACKUP_COUNT = 14
 LOG_ENCODING = "UTF-8"
