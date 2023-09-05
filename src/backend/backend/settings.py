e9e60a827ba674072b014c3c405a20b3f5a3a3e5
Marina Chernykh <106432563+MarinaChernykh@users.noreply.github.com>
2023-09-05 11:04:30 +0300

    add feature-test3-count-result function and test (#17)
    * Add a result calculation function for task 3
    
    * Add tests for this function

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -45,27 +45,33 @@ TEMPLATES = [
     },
 ]
 WSGI_APPLICATION = "backend.wsgi.application"
-if os.getenv("NEED_SQLITE"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": os.getenv(
-                "DB_ENGINE",
-                default="django.db.backends.postgresql_psycopg2",
-            ),
-            "NAME": os.getenv("POSTGRES_DB", default="postgres"),
-            "USER": os.getenv("POSTGRES_USER", default="postgres"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-            "HOST": os.getenv("DB_HOST", default="db"),
-            "PORT": os.getenv("DB_PORT", default=5432),
-        }
-    }
+}
+# if os.getenv("NEED_SQLITE"):
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": os.getenv(
+#                 "DB_ENGINE",
+#                 default="django.db.backends.postgresql_psycopg2",
+#             ),
+#             "NAME": os.getenv("POSTGRES_DB", default="postgres"),
+#             "USER": os.getenv("POSTGRES_USER", default="postgres"),
+#             "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+#             "HOST": os.getenv("DB_HOST", default="db"),
+#             "PORT": os.getenv("DB_PORT", default=5432),
+#         }
+#     }
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
