c67195ac847cc1c7878c0da56e972b6b59d65d8a
Даниил Паутов <danpautoff@yandex.ru>
2023-09-05 10:51:24 +0300

    Format code using pre-commit.

--- src/backend/api/calculation_service/task_3.py
+++ src/backend/api/calculation_service/task_3.py
@@ -1,49 +1,90 @@
 from api.models import Answer
 
-
 SCALES = {
-    '1а': 'scale_1', '1б': 'scale_2',
-    '2а': 'scale_1', '2б': 'scale_3',
-    '3а': 'scale_1', '3б': 'scale_4',
-    '4а': 'scale_1', '4б': 'scale_5',
-    '5а': 'scale_1', '5б': 'scale_6',
-    '6а': 'scale_2', '6б': 'scale_3',
-    '7а': 'scale_2', '7б': 'scale_4',
-    '8а': 'scale_2', '8б': 'scale_5',
-    '9а': 'scale_2', '9б': 'scale_6',
-    '10а': 'scale_3', '10б': 'scale_4',
-    '11а': 'scale_3', '11б': 'scale_5',
-    '12а': 'scale_3', '12б': 'scale_6',
-    '13а': 'scale_4', '13б': 'scale_5',
-    '14а': 'scale_4', '14б': 'scale_6',
-    '15а': 'scale_5', '15б': 'scale_6',
-    '16а': 'scale_1', '16б': 'scale_2',
-    '17а': 'scale_1', '17б': 'scale_3',
-    '18а': 'scale_4', '18б': 'scale_5',
-    '19а': 'scale_1', '19б': 'scale_4',
-    '20а': 'scale_2', '20б': 'scale_3',
-    '21а': 'scale_1', '21б': 'scale_6',
-    '22а': 'scale_2', '22б': 'scale_4',
-    '23а': 'scale_2', '23б': 'scale_5',
-    '24а': 'scale_2', '24б': 'scale_6',
-    '25а': 'scale_3', '25б': 'scale_5',
-    '26а': 'scale_3', '26б': 'scale_5',
-    '27а': 'scale_3', '27б': 'scale_6',
-    '28а': 'scale_1', '28б': 'scale_5',
-    '29а': 'scale_4', '29б': 'scale_6',
-    '30а': 'scale_5', '30б': 'scale_6',
-    '31а': 'scale_1', '31б': 'scale_2',
-    '32а': 'scale_1', '32б': 'scale_4',
-    '33а': 'scale_1', '33б': 'scale_5',
-    '34а': 'scale_1', '34б': 'scale_6',
-    '35а': 'scale_2', '35б': 'scale_4',
-    '36а': 'scale_2', '36б': 'scale_3',
-    '37а': 'scale_2', '37б': 'scale_6',
-    '38а': 'scale_3', '38б': 'scale_4',
-    '39а': 'scale_3', '39б': 'scale_5',
-    '40а': 'scale_4', '40б': 'scale_5',
-    '41а': 'scale_6', '41б': 'scale_3',
-    '42а': 'scale_4', '42б': 'scale_6',
+    "1а": "scale_1",
+    "1б": "scale_2",
+    "2а": "scale_1",
+    "2б": "scale_3",
+    "3а": "scale_1",
+    "3б": "scale_4",
+    "4а": "scale_1",
+    "4б": "scale_5",
+    "5а": "scale_1",
+    "5б": "scale_6",
+    "6а": "scale_2",
+    "6б": "scale_3",
+    "7а": "scale_2",
+    "7б": "scale_4",
+    "8а": "scale_2",
+    "8б": "scale_5",
+    "9а": "scale_2",
+    "9б": "scale_6",
+    "10а": "scale_3",
+    "10б": "scale_4",
+    "11а": "scale_3",
+    "11б": "scale_5",
+    "12а": "scale_3",
+    "12б": "scale_6",
+    "13а": "scale_4",
+    "13б": "scale_5",
+    "14а": "scale_4",
+    "14б": "scale_6",
+    "15а": "scale_5",
+    "15б": "scale_6",
+    "16а": "scale_1",
+    "16б": "scale_2",
+    "17а": "scale_1",
+    "17б": "scale_3",
+    "18а": "scale_4",
+    "18б": "scale_5",
+    "19а": "scale_1",
+    "19б": "scale_4",
+    "20а": "scale_2",
+    "20б": "scale_3",
+    "21а": "scale_1",
+    "21б": "scale_6",
+    "22а": "scale_2",
+    "22б": "scale_4",
+    "23а": "scale_2",
+    "23б": "scale_5",
+    "24а": "scale_2",
+    "24б": "scale_6",
+    "25а": "scale_3",
+    "25б": "scale_5",
+    "26а": "scale_3",
+    "26б": "scale_5",
+    "27а": "scale_3",
+    "27б": "scale_6",
+    "28а": "scale_1",
+    "28б": "scale_5",
+    "29а": "scale_4",
+    "29б": "scale_6",
+    "30а": "scale_5",
+    "30б": "scale_6",
+    "31а": "scale_1",
+    "31б": "scale_2",
+    "32а": "scale_1",
+    "32б": "scale_4",
+    "33а": "scale_1",
+    "33б": "scale_5",
+    "34а": "scale_1",
+    "34б": "scale_6",
+    "35а": "scale_2",
+    "35б": "scale_4",
+    "36а": "scale_2",
+    "36б": "scale_3",
+    "37а": "scale_2",
+    "37б": "scale_6",
+    "38а": "scale_3",
+    "38б": "scale_4",
+    "39а": "scale_3",
+    "39б": "scale_5",
+    "40а": "scale_4",
+    "40б": "scale_5",
+    "41а": "scale_6",
+    "41б": "scale_3",
+    "42а": "scale_4",
+    "42б": "scale_6",
 }
 TOP_RESULTS_NUMBER = 3
 
@@ -61,9 +102,7 @@ def calculate_task_3_result(user_answers: list[Answer]) -> str:
     return summary
 
 
-def _distribute_answers_by_scales(
-    user_answers: list[Answer]
-) -> dict[str: int]:
+def _distribute_answers_by_scales(user_answers: list[Answer]) -> dict[str:int]:
     """
     Считает кол-во ответов пользователя, относящихся к каждой из шкал.
     """
@@ -75,14 +114,14 @@ def _distribute_answers_by_scales(
 
 
 def _sorted_scales_in_non_growing_order(
-    scale_scores_counter: dict[str: int]
+    scale_scores_counter: dict[str:int],
 ) -> list[tuple[str, int]]:
     """
     Сортирует шкалы и кол-во ответов, относящихся к ним, по невозрастанию.
     """
     return sorted(
         scale_scores_counter.items(),
-        key=lambda scale_item: (-scale_item[1], scale_item[0])
+        key=lambda scale_item: (-scale_item[1], scale_item[0]),
     )
 
 
@@ -102,12 +141,8 @@ def _make_top_scales_by_scores(
     return user_top_features
 
 
-def _write_result_to_string(
-    user_top_features: list[tuple[str, int]]
-) -> str:
+def _write_result_to_string(user_top_features: list[tuple[str, int]]) -> str:
     """Формирует результат в виде текстовой строки."""
-    return ' '.join(
-        [':'.join([scale, str(score)])
-         for scale, score
-         in user_top_features]
+    return " ".join(
+        [":".join([scale, str(score)]) for scale, score in user_top_features]
     )
--- src/backend/api/calculation_service/test_of_task_3.py
+++ src/backend/api/calculation_service/test_of_task_3.py
@@ -10,6 +10,7 @@ class TestTask3(TestCase):
         Тестирует распределение ответов пользователя
         по шкалам и подсчет кол-ва баллов.
         """
+        # fmt: off
         test_user_answers = {
             1: 'а', 2: 'а', 3: 'б', 4: 'а', 5: 'б', 6: 'а', 7: 'б',
             8: 'а', 9: 'а', 10: 'б', 11: 'а', 12: 'б', 13: 'а', 14: 'б',
@@ -18,14 +19,18 @@ class TestTask3(TestCase):
             29: 'а', 30: 'а', 31: 'б', 32: 'а', 33: 'б', 34: 'а', 35: 'б',
             36: 'а', 37: 'а', 38: 'б', 39: 'а', 40: 'б', 41: 'а', 42: 'б',
         }
+        # fmt: on
         test_user_answers_objs = [
             Answer(number=number, content=content)
-            for number, content
-            in test_user_answers.items()
+            for number, content in test_user_answers.items()
         ]
         expected = {
-            'scale_1': 6, 'scale_4': 9, 'scale_6': 7,
-            'scale_2': 9, 'scale_3': 5, 'scale_5': 6
+            "scale_1": 6,
+            "scale_4": 9,
+            "scale_6": 7,
+            "scale_2": 9,
+            "scale_3": 5,
+            "scale_5": 6,
         }
         received = task_3._distribute_answers_by_scales(test_user_answers_objs)
         self.assertEquals(received, expected)
@@ -35,31 +40,62 @@ class TestTask3(TestCase):
         Тестирует корректность сортировки словаря с кол-вом ответов по шкалам.
         """
         test_scale_scores_counters = {
-            'all_scores_different': {
-                'scale_2': 8, 'scale_5': 9, 'scale_4': 10,
-                'scale_6': 4, 'scale_1': 6, 'scale_3': 5
+            "all_scores_different": {
+                "scale_2": 8,
+                "scale_5": 9,
+                "scale_4": 10,
+                "scale_6": 4,
+                "scale_1": 6,
+                "scale_3": 5,
             },
-            'some_scores_equals': {
-                'scale_4': 8, 'scale_1': 10, 'scale_3': 3,
-                'scale_2': 8, 'scale_6': 9, 'scale_5': 4
+            "some_scores_equals": {
+                "scale_4": 8,
+                "scale_1": 10,
+                "scale_3": 3,
+                "scale_2": 8,
+                "scale_6": 9,
+                "scale_5": 4,
             },
-            'all_scores_equals': {
-                'scale_5': 7, 'scale_2': 7, 'scale_1': 7,
-                'scale_3': 7, 'scale_4': 7, 'scale_6': 7
-            }
+            "all_scores_equals": {
+                "scale_5": 7,
+                "scale_2": 7,
+                "scale_1": 7,
+                "scale_3": 7,
+                "scale_4": 7,
+                "scale_6": 7,
+            },
         }
         expected = [
-            [('scale_4', 10), ('scale_5', 9), ('scale_2', 8),
-             ('scale_1', 6), ('scale_3', 5), ('scale_6', 4),],
-            [('scale_1', 10), ('scale_6', 9), ('scale_2', 8),
-             ('scale_4', 8), ('scale_5', 4), ('scale_3', 3),],
-            [('scale_1', 7), ('scale_2', 7), ('scale_3', 7),
-             ('scale_4', 7), ('scale_5', 7), ('scale_6', 7),],
+            [
+                ("scale_4", 10),
+                ("scale_5", 9),
+                ("scale_2", 8),
+                ("scale_1", 6),
+                ("scale_3", 5),
+                ("scale_6", 4),
+            ],
+            [
+                ("scale_1", 10),
+                ("scale_6", 9),
+                ("scale_2", 8),
+                ("scale_4", 8),
+                ("scale_5", 4),
+                ("scale_3", 3),
+            ],
+            [
+                ("scale_1", 7),
+                ("scale_2", 7),
+                ("scale_3", 7),
+                ("scale_4", 7),
+                ("scale_5", 7),
+                ("scale_6", 7),
+            ],
         ]
         for test_name, expected in zip(test_scale_scores_counters, expected):
             with self.subTest(test_name=test_name):
                 received = task_3._sorted_scales_in_non_growing_order(
-                    test_scale_scores_counters[test_name])
+                    test_scale_scores_counters[test_name]
+                )
                 self.assertEquals(received, expected)
 
     def test_make_top_scales_by_scores(self):
@@ -67,41 +103,58 @@ class TestTask3(TestCase):
         Тестирует выбор шкал с наибольшим кол-вом баллов (топ 3).
         """
         test_scale_scores_counters = {
-            '3_leaders': [
-                ('scale_3', 10), ('scale_1', 9), ('scale_5', 8),
-                ('scale_4', 6), ('scale_2', 5), ('scale_6', 4),
+            "3_leaders": [
+                ("scale_3", 10),
+                ("scale_1", 9),
+                ("scale_5", 8),
+                ("scale_4", 6),
+                ("scale_2", 5),
+                ("scale_6", 4),
             ],
-            '4 leaders': [
-                ('scale_1', 10), ('scale_3', 9), ('scale_2', 8),
-                ('scale_6', 8), ('scale_4', 4), ('scale_5', 3),
+            "4 leaders": [
+                ("scale_1", 10),
+                ("scale_3", 9),
+                ("scale_2", 8),
+                ("scale_6", 8),
+                ("scale_4", 4),
+                ("scale_5", 3),
             ],
-            'all_scores_equals': [
-                ('scale_1', 7), ('scale_2', 7), ('scale_3', 7),
-                ('scale_4', 7), ('scale_5', 7), ('scale_6', 7),
+            "all_scores_equals": [
+                ("scale_1", 7),
+                ("scale_2", 7),
+                ("scale_3", 7),
+                ("scale_4", 7),
+                ("scale_5", 7),
+                ("scale_6", 7),
             ],
         }
         expected = [
+            [("scale_3", 10), ("scale_1", 9), ("scale_5", 8)],
             [
-                ('scale_3', 10), ('scale_1', 9), ('scale_5', 8)
+                ("scale_1", 10),
+                ("scale_3", 9),
+                ("scale_2", 8),
+                ("scale_6", 8),
             ],
             [
-                ('scale_1', 10), ('scale_3', 9), ('scale_2', 8),
-                ('scale_6', 8),
-            ],
-            [
-                ('scale_1', 7), ('scale_2', 7), ('scale_3', 7),
-                ('scale_4', 7), ('scale_5', 7), ('scale_6', 7),
+                ("scale_1", 7),
+                ("scale_2", 7),
+                ("scale_3", 7),
+                ("scale_4", 7),
+                ("scale_5", 7),
+                ("scale_6", 7),
             ],
         ]
         for test_name, expected in zip(test_scale_scores_counters, expected):
             with self.subTest(test_name=test_name):
                 received = task_3._make_top_scales_by_scores(
-                    test_scale_scores_counters[test_name])
+                    test_scale_scores_counters[test_name]
+                )
                 self.assertEquals(received, expected)
 
     def test_write_result_to_string(self):
         """Проверяет корректность формирования результирующей строки."""
-        top_scores_scale = [('scale_3', 10), ('scale_1', 9), ('scale_5', 8)]
-        expected = 'scale_3:10 scale_1:9 scale_5:8'
+        top_scores_scale = [("scale_3", 10), ("scale_1", 9), ("scale_5", 8)]
+        expected = "scale_3:10 scale_1:9 scale_5:8"
         received = task_3._write_result_to_string(top_scores_scale)
         self.assertEquals(received, expected)
