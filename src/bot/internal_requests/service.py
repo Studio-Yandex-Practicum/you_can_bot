18d8f88f23356067a9730e534634eea3102efcc8
Никита Остапенко <nikitaostapenko0968@gmail.com>
2023-09-12 22:48:18 +0300

    complete request for api

--- src/bot/internal_requests/service.py
+++ src/bot/internal_requests/service.py
@@ -1,27 +1,57 @@
-from dataclasses import asdict
 import logging
+from dataclasses import asdict
 from urllib.parse import urljoin
 
 from httpx import AsyncClient, Response
 
-from internal_requests.entities import (
-    Answer, Problem, UserFromTelegram, TaskStatus, Message)
+from internal_requests.entities import Answer, Problem, TaskStatus, UserFromTelegram
 from utils.configs import INTERNAL_API_URL
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def get_message_with_question(task: TaskStatus) -> Response:
+    """Получение сообщения с содержанием вопроса"""
     endpoint_urn = f"task/{task.task_number}/question/{task.question_number}/"
     response = await _get_request(endpoint_urn)
-    return response.text
+    return response
 
-  
+
+async def get_result(result: TaskStatus) -> Response:
+    """Получение сообщения с расшифровкой."""
+    endpoint_urn = (
+        f"api/v1/users/{result.telegram_id}/tasks/{result.task_number}/results/"
+    )
+    response = await _get_request(endpoint_urn)
+    return response
+
+
 async def create_user(user: UserFromTelegram) -> Response:
     """Создания пользователя."""
     data = asdict(user)
     endpoint_urn = "users/"
     response = await _post_request(data, endpoint_urn)
+    return response
+
+
+async def get_info_about_user(user: UserFromTelegram) -> Response:
+    """Получения информации о пользователе."""
+    endpoint_urn = "users/"
+    response = await _get_request(endpoint_urn)
+    return response
+
+
+async def get_list_task_status() -> Response:
+    """Получение списка статусов заданий."""
+    endpoint_urn = "task/"
+    response = await _get_request(endpoint_urn)
+    return response
+
+
+async def get_concretre_task_status(task: TaskStatus) -> Response:
+    """Получение информации о конкретном статусе задания."""
+    endpoint_urn = f"task/{task.task_number}/"
+    response = await _get_request(endpoint_urn)
     return response
 
 
@@ -50,6 +80,7 @@ async def _get_request(endpoint_urn: str) -> Response:
             )
         )
     await _log_get_response(response)
+    return response.json()
 
 
 async def _log_get_response(response: Response) -> None:
