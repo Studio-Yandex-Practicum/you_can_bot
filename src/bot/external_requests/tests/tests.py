b6208f9fe485bd2e951175106979fecfb118783a
horus <aleksey.nagornov@yandex.ru>
2023-09-19 18:48:04 +0200

    refactoring tests

--- src/bot/external_requests/tests/tests.py
+++ src/bot/external_requests/tests/tests.py
@@ -1,4 +1,5 @@
 from http.client import FORBIDDEN, NOT_FOUND
+from typing import Sequence
 from unittest.mock import MagicMock, patch
 
 from httpx import AsyncClient, HTTPStatusError, Request, RequestError, Response, codes
@@ -12,13 +13,14 @@ class TestGetUserInfoFromLK(CaseForGetUserInfoFromLK):
 
     def setUp(self) -> None:
         self.response = MagicMock()
+        self.response.status_code = codes.OK
 
-    def set_mock(
+    def set_post_request(
         self,
         post_request: MagicMock,
         exception: Exception,
         **kwargs
-    ):
+    ) -> None:
         """Мок с переданным исключением, сообщением и ответом.
         ### Args:
         - post_request (MagicMock):
@@ -35,10 +37,48 @@ class TestGetUserInfoFromLK(CaseForGetUserInfoFromLK):
             side_effect=exception(request=Request('POST', self.FAKE_URL), **kwargs)
         )
 
+    async def assert_equal_from_sequence(
+        self,
+        post_request: MagicMock,
+        sequence: Sequence[tuple[dict, Exception]]
+    ) -> None:
+        """Шаблон для вызова assertEqual из последовательности.
+        ### Args:
+        - post_request (MagicMock):
+            Мок
+        - sequence (Sequence[tuple[dict, Exception]]):
+          последовательность кортежей с подменяемыми словарями
+          и ожидаемыми значениями.
+        """
+        for return_value, expected in sequence:
+            with self.subTest(return_value=return_value, expected=expected):
+                post_request.return_value = return_value
+                user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
+                self.assertEqual(user_info, expected)
+
+    async def assert_raises_from_sequence(
+        self,
+        post_request: MagicMock,
+        sequence: Sequence[tuple[dict, Exception]]
+    ) -> None:
+        """Шаблон для вызова assertRaises из последовательности.
+        ### Args:
+        - post_request (MagicMock):
+            Мок
+        - sequence (Sequence[tuple[dict, Exception]]):
+          последовательность кортежей с подменяемыми словарями
+          и ожидаемыми исключениями.
+        """
+        for return_value, exception in sequence:
+            with self.subTest(return_value=return_value, exception=exception):
+                post_request.return_value = return_value
+                with self.assertRaises(exception):
+                    await get_user_info_from_lk(self.TELEGRAM_ID)
+
     @patch.object(AsyncClient, 'post')
     async def test_403_http_error(self, post_request):
         """Тест ошибки 403 Forbidden."""
-        self.set_mock(
+        self.set_post_request(
             post_request=post_request,
             exception=HTTPStatusError,
             message=self.MESSAGE_403_FORBIDDEN,
@@ -50,7 +90,7 @@ class TestGetUserInfoFromLK(CaseForGetUserInfoFromLK):
     @patch.object(AsyncClient, 'post')
     async def test_404_http_error(self, post_request):
         """Тест ошибки 404 Not Found."""
-        self.set_mock(
+        self.set_post_request(
             post_request=post_request,
             exception=HTTPStatusError,
             message=self.MESSAGE_404_NOT_FOUND,
@@ -62,32 +102,32 @@ class TestGetUserInfoFromLK(CaseForGetUserInfoFromLK):
     @patch.object(AsyncClient, 'post')
     async def test_incorrect_json(self, post_request):
         """Тест отказа сервера с кодом code в json."""
-        self.response.status_code = codes.OK
-        self.response.json = MagicMock(return_value=self.ERROR_JSON)
-        post_request.return_value = self.response
-        with self.assertRaises(PostAPIError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
+        self.response.json = MagicMock(return_value=self.ERROR_CODE)
+        await self.assert_raises_from_sequence(
+            post_request,
+            [(self.response, PostAPIError)]
+        )
 
     async def test_incorrect_telegram_id(self):
         """Тест некорректного телеграм id."""
         with self.assertRaises(TypeError):
             await get_user_info_from_lk(self.STRING)
         with self.assertRaises(ValueError):
-            await get_user_info_from_lk(self.NEGATIVE_INT)
+            await get_user_info_from_lk(self.NEGATIVE_NUMBER)
 
     @patch.object(AsyncClient, 'post')
     async def test_incorrect_type_in_responce(self, post_request):
         """Тест некорректного содержания json."""
-        self.response.status_code = codes.OK
         self.response.json = MagicMock(return_value=self.STRING)
-        post_request.return_value = self.response
-        with self.assertRaises(TypeError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
+        await self.assert_raises_from_sequence(
+            post_request,
+            [(self.response, TypeError)]
+        )
 
     @patch.object(AsyncClient, 'post')
     async def test_request_error(self, post_request):
         """Тест сбоя сети."""
-        self.set_mock(
+        self.set_post_request(
             post_request=post_request,
             exception=RequestError,
             message=self.MESSAGE_REQUEST_ERROR
@@ -98,68 +138,56 @@ class TestGetUserInfoFromLK(CaseForGetUserInfoFromLK):
     @patch.object(AsyncClient, 'post')
     async def test_success_request(self, post_request):
         """Тест успешного запроса."""
-        self.response.status_code = codes.OK
-        self.response.json = MagicMock(return_value=self.SUCCESS_JSON)
-        post_request.return_value = self.response
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertEqual(user_info, self.SUCCESS_USER_INFO)
+        self.response.json = MagicMock(return_value=self.SUCCESS_DATA)
+        await self.assert_equal_from_sequence(
+            post_request, [(self.response, self.SUCCESS_USER_INFO)]
+        )
 
     @patch('external_requests.service._post_request')
     async def test_unexpected_full_name(self, post_request):
         """Тест неожиданных значений full_name."""
-        post_request.return_value = self.JSON_WITHOUT_SURNAME
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertEqual(user_info, self.RETURN_WITHOUT_SURNAME)
-        post_request.return_value = self.JSON_WITH_DOUBLE_SPACE_IN_NAME
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertEqual(user_info, self.RETURN_WITH_DOUBLE_SPACE_IN_NAME)
+        await self.assert_equal_from_sequence(
+            post_request, [
+                (self.NO_SURNAME, self.RETURN_NO_SURNAME),
+                (self.DOUBLE_SPACE_IN_NAME, self.RETURN_DOUBLE_SPACE_IN_NAME)
+            ]
+        )
 
     @patch('external_requests.service._post_request')
     async def test_upsent_json_keys(self, post_request):
         """Тест отсутствия ключей json."""
-        post_request.return_value = self.JSON_WITHOUT_TARIFF
-        with self.assertRaises(KeyError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
-        post_request.return_value = self.JSON_WITHOUT_FULL_NAME
-        with self.assertRaises(KeyError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
-        post_request.return_value = self.JSON_WITHOUT_ISAPPROVED
-        with self.assertRaises(KeyError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
+        await self.assert_raises_from_sequence(
+            post_request, [
+                (self.NO_IS_APPROVED, KeyError),
+                (self.NO_FULL_NAME, KeyError),
+                (self.NO_TARIFF, KeyError)
+            ]
+        )
 
     @patch('external_requests.service._post_request')
     async def test_unexpected_json_values(self, post_request):
         """Тест неожиданных значений json."""
-        post_request.return_value = self.JSON_UNEXPECTED_TYPE_TARIFF
-        with self.assertRaises(ValueError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
-        post_request.return_value = self.JSON_UNEXPECTED_TYPE_FULL_NAME
-        with self.assertRaises(TypeError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
-        post_request.return_value = self.JSON_UNEXPECTED_TYPE_ISAPPROVED
-        with self.assertRaises(TypeError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
+        await self.assert_raises_from_sequence(
+            post_request, [
+                (self.UNEXPECTED_TYPE_ISAPPROVED, TypeError),
+                (self.UNEXPECTED_TYPE_FULL_NAME, TypeError),
+                (self.UNEXPECTED_TYPE_TARIFF, ValueError)
+            ]
+        )
 
     @patch.object(AsyncClient, 'post')
     async def test_unexpected_status_code(self, post_request):
-        """Тест неожиданных значений status_code."""
+        """Тест неожиданного значения status_code."""
         self.response.status_code = self.UNEXPECTED_STATUS_CODE
-        post_request.return_value = self.response
-        with self.assertRaises(PostAPIError):
-            await get_user_info_from_lk(self.TELEGRAM_ID)
+        await self.assert_raises_from_sequence(
+            post_request, [(self.response, PostAPIError)]
+        )
 
     @patch('external_requests.service._post_request')
-    async def test_tariff(self, post_request):
+    async def test_tariff_value(self, post_request):
         """Тест значений tariff в json."""
-        post_request.return_value = self.JSON_TARIFF_MINI
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertIn(user_info['tariff'], TARIFFS)
-        post_request.return_value = self.JSON_TARIFF_MIDI
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertIn(user_info['tariff'], TARIFFS)
-        post_request.return_value = self.JSON_TARIFF_MAXI
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertIn(user_info['tariff'], TARIFFS)
-        post_request.return_value = self.JSON_TARIFF_NONE
-        user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
-        self.assertIn(user_info['tariff'], TARIFFS)
+        for value in [self.TARIFF_MAXI, self.TARIFF_MIDI,
+                      self.TARIFF_MINI, self.TARIFF_NONE]:
+            post_request.return_value = value
+            user_info = await get_user_info_from_lk(self.TELEGRAM_ID)
+            self.assertIn(user_info['tariff'], TARIFFS)
