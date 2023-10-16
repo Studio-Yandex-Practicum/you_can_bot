7448552a7e891551c6fd4c3c7a5f5ad5b139f3e6
pomogashkin <noskovdaniks@gmail.com>
2023-10-16 13:51:15 +0300

    fixes and new total/Users/pomogashkin/Dev/you_can_bot/.venv/bin/python

--- src/backend/api/tests/test_problem/fixtures.py
+++ src/backend/api/tests/test_problem/fixtures.py
@@ -1,12 +1,15 @@
 from rest_framework.test import APITestCase
+from django.contrib.auth.hashers import make_password
+from django.db import transaction
 
-from api.models import UserFromTelegram
+from api.models import MentorProfile, User, UserFromTelegram
 
 
 class BaseCaseForProblemTests(APITestCase):
     """Базовый набор констант для тестов модуля api."""
 
     TELEGRAM_ID = 1234567
+    MENTOR_TELEGRAM_ID = 7654321
     UNKNOWN_USER_TELEGRAM_ID = 7654321
     TELEGRAM_USERNAME = "hasnoname"
     TELEGRAM_NAME = "HasNoName"
@@ -14,13 +17,22 @@ class BaseCaseForProblemTests(APITestCase):
     MESSAGE = "Как дела?"
     EMPTY_MESSAGE_1 = ""
     EMPTY_MESSAGE_2 = " "
+    DJANGO_USERNAME = "username"
+    DJANGO_USER_PASSWORD = "password"
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user_from_telegram = UserFromTelegram.objects.create(
-            telegram_id=cls.TELEGRAM_ID,
-            telegram_username=cls.TELEGRAM_USERNAME,
-            name=cls.TELEGRAM_NAME,
-            surname=cls.TELEGRAM_SURNAME,
-        )
+        with transaction.atomic():
+            cls.user = User.objects.create_user(
+                username=cls.DJANGO_USERNAME,
+            )
+            cls.user.mentorprofile.telegram_id = cls.MENTOR_TELEGRAM_ID
+            cls.user.mentorprofile.save()
+            print(f"{cls.user.mentorprofile.telegram_id=}")
+            cls.user_from_telegram = UserFromTelegram.objects.create(
+                telegram_id=cls.TELEGRAM_ID,
+                telegram_username=cls.TELEGRAM_USERNAME,
+                name=cls.TELEGRAM_NAME,
+                surname=cls.TELEGRAM_SURNAME,
+            )
