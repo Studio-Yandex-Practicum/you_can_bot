72cdf93bdd27eacc083e2b34579d4124534c2e0c
Daniil Pautov <danpautoff@yandex.ru>
2025-06-06 13:48:52 +0300

    Add test-summary field with WYSIWYG editor and PDF display. (#164)

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("tinymce/", include("tinymce.urls")),
     path("admin/", include("info_pages.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
